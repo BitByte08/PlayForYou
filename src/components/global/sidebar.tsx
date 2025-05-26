a7bc7ffd8c2680ee9e079002dce6e386abe119f9
bitbyte08 <eight2823@naver.com>
2025-05-26 12:09:42 -0700

    feat :: 코드 정리

--- src/components/global/sidebar.tsx
+++ src/components/global/sidebar.tsx
@@ -1,8 +1,9 @@
 'use client';
-import { useState, useEffect } from 'react';
-import { IoCreateOutline, IoPersonOutline } from "react-icons/io5";
+import {useState, useEffect} from 'react';
+import {IoCreateOutline, IoPersonOutline} from "react-icons/io5";
 import {Modal} from "@/components/global/modal";
 import Link from "next/link";
+import {useModalStore} from "@/stores/modalStore";
 
 interface SectionProps {
   href: string;
@@ -10,31 +11,35 @@ interface SectionProps {
 }
 
 export const Sidebar = () => {
-    useEffect(() => {
-        setTimeout(()=>{
-            setIsSidebarOpen(true);
-        },2000);
-    },[]);
-    const [isSidebarOpen, setIsSidebarOpen] = useState(true);
-    return (
-        <div className={`h-full ${isSidebarOpen?"w-128":" w-26"} transition-all duration-500 flex flex-col gap-4`}
-             onMouseEnter={() => setIsSidebarOpen(true)}
-             onMouseLeave={() => setIsSidebarOpen(true)}>
-            <section className={`h-[calc(100%-7.5rem)] border-default rounded-2xl border-1 transition-all duration-500 flex flex-col p-6 background-default`}
-                    id="sidebar"
-            >
-              <Section href={"/login"} >
-                <IoPersonOutline className="h-12 w-12 p-1" />
-                사용자
-              </Section>
-              <Section href={"/room"} >
-                <IoCreateOutline className="h-12 w-12 pl-1.5 pr-0.5 pt-1 pb-1" />
-                방 생성하기
-              </Section>
-            </section>
-            <Modal />
-        </div>
-    )
+  const modal = useModalStore(state => state.state);
+  const modalHandle = (state:boolean) => {
+    if (modal) setIsSidebarOpen(true);
+    else setIsSidebarOpen(state);
+  }
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  useEffect(() => {
+    modalHandle(true);
+  }, [modal]);
+  return (
+    <div className={`h-full ${isSidebarOpen ? "w-128" : " w-26"} transition-all duration-500 flex flex-col gap-4`}
+         onMouseEnter={() => modalHandle(true)}
+         onMouseLeave={() => modalHandle(false)}>
+      <section
+        className={`h-[calc(100%-7.5rem)] border-default rounded-2xl border-1 transition-all duration-500 flex flex-col p-6 background-default`}
+        id="sidebar"
+      >
+        <Section href={"/login"}>
+          <IoPersonOutline className="h-12 w-12 p-1"/>
+          사용자
+        </Section>
+        <Section href={"/room"}>
+          <IoCreateOutline className="h-12 w-12 pl-1.5 pr-0.5 pt-1 pb-1"/>
+          방 생성하기
+        </Section>
+      </section>
+      <Modal/>
+    </div>
+  )
 }
 
 const Section = (props: SectionProps) => {
