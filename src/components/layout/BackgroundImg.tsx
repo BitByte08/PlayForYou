f64d2927de98073ce8e270cef16c3ef59af140af
bitbyte08 <me@bitworkspace.kr>
2025-06-25 00:10:35 +0900

    feat :: 언스플데시를 이용한 렌덤 배경 구현

--- src/components/layout/BackgroundImg.tsx
+++ src/components/layout/BackgroundImg.tsx
@@ -1,9 +1,28 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import Image from "next/image";
 
 const BackgroundImg = () => {
+	const [bgUrl, setBgUrl] = useState("");
+
+	useEffect(() => {
+		fetch("/api/background")
+			.then((res) => res.json())
+			.then((data) => setBgUrl(data.url));
+	}, []);
+
+	if (!bgUrl) return null;
+
 	return (
-		<Image src="" alt="BackgroundImg" className="fixed left-0 top-0 w-full h-full h-full z-0 " />
-	)
-}
+		<Image
+			src={bgUrl}
+			alt="BackgroundImg"
+			fill
+			unoptimized
+			className="fixed left-0 top-0 w-full h-full object-cover z-0"
+		/>
+	);
+};
 
 export default BackgroundImg;
